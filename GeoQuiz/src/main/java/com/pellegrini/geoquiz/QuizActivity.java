ea79e4e661db3b3aff77332504e3b9eda12a6c3e
fabricio.pellegrini@gmail.com <pikachu2107>
2013-08-15 21:52:45 -0300

    Capitulo 1 - First Android Application

--- GeoQuiz/src/main/java/com/pellegrini/geoquiz/QuizActivity.java
+++ GeoQuiz/src/main/java/com/pellegrini/geoquiz/QuizActivity.java
@@ -3,13 +3,37 @@ package com.pellegrini.geoquiz;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends Activity {
+
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button) findViewById(R.id.button_true);
+        mFalseButton = (Button) findViewById(R.id.button_false);
+
+        mTrueButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mFalseButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
